3b884afa218b51770b64597a512c3a3b2967d45a
julio <julio@drecon.com.br>
2013-07-07 16:21:01 -0300

    melhoria no nomes

--- app/views/layouts/menu.blade.php
+++ app/views/layouts/menu.blade.php
@@ -3,9 +3,8 @@
 	<a class="brand" href="#">Job4Fun</a>
 	<ul class="nav">
 		<li><a href="{{ URL::to('hero/index') }}">Hero</a></li>
-		<li><a href="#">Campaigns</a></li>
-		<li><a href="#">Premiuns</a></li>
-		<li><a href="#">Feeds</a></li>
+		<li><a href="#">Customers</a></li>
+		<li><a href="#">Contact</a></li>
 	</ul>
 	<div class="navbar-form pull-right">
 		<a href="{{ URL::to('logout') }}" type="submit" class="btn">Logout</a>
